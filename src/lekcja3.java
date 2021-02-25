510f06443b4d623abead2189d5031eb10326fafd
adriansobierajski <79514965+adriansobierajski@users.noreply.github.com>
2021-02-25 13:09:48 +0100

    lekcja3

---
+++ src/lekcja3.java
@@ -0,0 +1,25 @@
+public class lekcja3 {
+    public static void main(String[] args) {
+        //zadanie 1
+        int intA =3;
+        double doubleA =1.65;
+        char CharA = '!';
+        String StringA = " jest wieksze od ";
+        System.out.println(intA + StringA + doubleA + CharA);
+//zadanie 2
+        int A = 6;
+        int B = 5;
+int sum = (A*A)+(B*B);
+        System.out.println("wynik:"+sum);
+         String myStringA = "Adrian";
+        String myStringB = "Sobierajski";
+        System.out.println(myStringA + "\n" + myStringB);
+
+    }
+
+
+
+
+
+
+}
